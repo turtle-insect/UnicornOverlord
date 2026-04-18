cc3814af3db1167602b32dff5db83147e7c09349
turtle-insect <turtle.insect@gmail.com>
2026-04-18 14:47:27 +0900

    refactor

--- UnicornOverlord/ViewModel.cs
+++ UnicornOverlord/ViewModel.cs
@@ -1,14 +1,6 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace UnicornOverlord
